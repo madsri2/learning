96d50196655e3aeed698c87c80955e84303b0e94
madsri2 <madsri2@gmail.com>
2016-03-20 10:50:45 -0700

    adding first set of views

--- learning/travel/models.py
+++ learning/travel/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import datetime
 
 # Create your models here.
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
+
+
+class User(models.Model):
+	user_name = models.CharField(max_length=20)
+
+	def __str__(self):
+        	return self.user_name
+
+class Itinerary(models.Model):
+	user=models.ForeignKey(User, on_delete=models.CASCADE)
+	source_city=models.CharField(max_length=50)
+	dest_city=models.CharField(max_length=50)
+	start_date=models.DateTimeField()
+	num_days=models.IntegerField(default=0)
+	
+	def __str__(self):
+        	return self.user.user_name + ":" + self.source_city + " to " + self.dest_city 
+
+	def travelling_soon(self):
+		return self.start_date >= timezone.now() + datetime.timedelta(days=20)
--- learning/travel/views.py
+++ learning/travel/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("First step for me!")
+
+def destination(request):
+	return HttpResponse("You will enter countries, cities and places you want to visit here")
+
+def itinerary(request):
+	return HttpResponse("This is where your itinerary will be listed")
